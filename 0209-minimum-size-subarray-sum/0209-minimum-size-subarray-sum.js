ff9d4b0bee1d3d7fdcbc2335c9f1b3f2986daace
Akash Kadam <113874766+Akash-kadam14@users.noreply.github.com>
2025-06-20 20:17:57 +0530

    Time: 401 ms (5.02%), Space: 69.9 MB (5.1%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
@@ -5,7 +5,7 @@
  */
 var minSubArrayLen = function (target, nums) {
     let left = 0;
-    let res = Number.MAX_SAFE_INTEGER;;
+    let res = Infinity
     let winSum = 0
     for (let right = 0; right < nums.length; right++) {
         winSum += nums[right];
@@ -17,5 +17,5 @@ var minSubArrayLen = function (target, nums) {
         }
 
     }
-    return res === Number.MAX_SAFE_INTEGER ? 0 : res;
+    return res === Infinity ? 0 : res;
 };
